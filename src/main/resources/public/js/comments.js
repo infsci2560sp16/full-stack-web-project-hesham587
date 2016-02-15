0ea25e18b2c068f72f3f932cdbed68c502aaec0b
hesham587 <hisham.alhulayel@gmail.com>
2016-02-15 16:56:30 -0500

    try9

--- src/main/resources/public/js/comments.js
+++ src/main/resources/public/js/comments.js
@@ -14,20 +14,41 @@ function Addcomment(){
 function addRow() {
 
 
+
    var getCommentBox= document.getElementById("commentText").value;
+   if (myTrim(getCommentBox)===''){
+	   window.alert("Type a comment");
+	   
+   }
+   else{
+	   
+   
     var div = document.createElement('div');
 
     div.className = 'class="col-9">';
 
-    div.innerHTML =getCommentBox + '\<button class="button button-block" value="Delete comment" onclick="removeRow(this)">Delete comment </button>';
+    div.innerHTML ='\<button class="button button-block" value="Delete comment" onclick="removeRow(this)">Delete comment </button>' + getCommentBox;
 
      document.getElementById('comments').appendChild(div);
+	Rest();
+	 }
 	 }
 	
 
 
 function removeRow(input) {
     document.getElementById('comments').removeChild( input.parentNode );
+}
+
+function Rest() {
+	var getCommentBox= document.getElementById("commentText");
+	getCommentBox.value='';
+	
+	
+}
+
+function myTrim(x) {
+    return x.replace(/^\s+|\s+$/gm,'');
 }
 
 	
