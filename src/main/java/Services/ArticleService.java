87615c71602ed553c46ccffe740645eabebcf5e0
hesham587 <hisham.alhulayel@gmail.com>
2016-03-27 22:14:42 -0400

    ss

--- src/main/java/Services/ArticleService.java
+++ src/main/java/Services/ArticleService.java
@@ -82,7 +82,7 @@ public class ArticleService {
         
         arrayList = new ArrayList<>();
         int length = 2;
-        length = length + 5;
+      //  length = length + 5;
         for (int i = 0; i < length; i++) {
              
             article = new HashMap<String, String>();
